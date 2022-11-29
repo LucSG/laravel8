7117b0de2db17695bd1976ca60055c566726eb7a
LucSG <lucasguima@yahoo.com.br>
2022-11-29 11:22:11 -0500

    Validacao de dados

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,8 +1,19 @@
 
 <h1>Cadastra Novo Post</h1>
+
+@if ($errors->any())
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{$error}}</li>
+        @endforeach
+    </ul>
+    
+@endif
+
+
 <form action="{{ route('posts.store')}}" method="post">
     @csrf
-    <input type="text" name="title" id="title" placeholder="Titulo">
-    <textarea name="content" id="content" cols="30" rows="4" placeholder="Conteúdo"></textarea>
+    <input type="text" name="title" id="title" placeholder="Titulo"  value="{{old('title')}}">
+    <textarea name="content" id="content" cols="30" rows="4" placeholder="Conteúdo">{{ old('content')}}</textarea>
     <button type="submit">Enviar</button>
 </form>
