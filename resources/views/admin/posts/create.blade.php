fd258a208de2f88f4bdce5eddb590f8f37950858
LucSG <lucasguima@yahoo.com.br>
2022-11-29 00:31:36 -0500

    Rota para Criar BD

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,7 @@
+
+<h1>Cadastra Novo Post</h1>
+<form action="" method="post">
+    <input type="text" name="title" id="title" placeholder="Titulo">
+    <textarea name="content" id="content" cols="30" rows="4" placeholder="Conteúdo"></textarea>
+    <button type="submit">Enviar</button>
+</form>
